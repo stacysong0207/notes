ca0bbdc5bdb8f98bd484bcdac0a41bb140315c23
stanleyguo0207 <stanleyguo0207@163.com>
2018-09-30 17:30:56 +0800

    [apue]代码格式修改2

--- apue/code/src/c_00002_pathname.c
+++ apue/code/src/c_00002_pathname.c
@@ -3,12 +3,12 @@
 #include <limits.h>
 
 #ifdef PATH_MAX
-    static long pathmax = PATH_MAX;
+static long pathmax = PATH_MAX;
 #else
-    static long pathmax = 0;
+static long pathmax = 0;
 #endif
-    static long posix_version = 0;
-    static long xsi_version = 0;
+static long posix_version = 0;
+static long xsi_version = 0;
 
 /* If PATH_MAX is indeterminate, no guarantee this is adequate */
 #define PATH_MAX_GUESS 1024
@@ -19,18 +19,22 @@ path_alloc(size_t *sizep) /* also return allocated size, if nonnull */
     char *ptr;
     size_t size;
 
-    if(posix_version == 0)
+    if (posix_version == 0)
         posix_version = sysconf(_SC_VERSION);
     if (xsi_version == 0)
         xsi_version = sysconf(_SC_XOPEN_VERSION);
-    if (pathmax == 0) { /* first time through */
+    if (pathmax == 0)
+    { /* first time through */
         errno = 0;
-        if ((pathmax = pathconf("/", _PC_PATH_MAX)) < 0) {
+        if ((pathmax = pathconf("/", _PC_PATH_MAX)) < 0)
+        {
             if (errno == 0)
                 pathmax = PATH_MAX_GUESS; /* it��s indeterminate */
             else
                 err_sys("pathconf error for _PC_PATH_MAX");
-        } else {
+        }
+        else
+        {
             pathmax++; /* add one since it��s relative to root */
         }
     }
@@ -46,5 +50,5 @@ path_alloc(size_t *sizep) /* also return allocated size, if nonnull */
         err_sys("malloc error for pathname");
     if (sizep != NULL)
         *sizep = size;
-    return(ptr);
+    return (ptr);
 }
