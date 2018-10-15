67bd13356c4a755a6b834bfb6850e4119e4caf7c
stanleyguo0207 <stanleyguo0207@163.com>
2018-10-15 12:36:25 +0800

    [apue]I/O lseek
    测试对其标准输入能否设置偏移量

---
+++ apue/code/src/c_00007_io_lseek.c
@@ -0,0 +1,14 @@
+#include "apue.h"
+
+int
+main(int argc, char *argv[])
+{
+    if(-1 == lseek(STDIN_FILENO, 0, SEEK_CUR)) {
+        printf("cannot seek\n");
+    } else {
+        printf("seek OK\n");
+    }
+
+    exit(0);
+}
+
