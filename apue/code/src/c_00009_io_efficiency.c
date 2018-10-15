5362e56814b6f57f113377e442284caae69f2063
stanleyguo0207 <stanleyguo0207@163.com>
2018-10-15 16:29:26 +0800

    [apue]I/O 效率
    将标准输入复制到标准输出

---
+++ apue/code/src/c_00009_io_efficiency.c
@@ -0,0 +1,23 @@
+#include "apue.h"
+
+#define BUFFSIZE 4096
+
+int
+main(int argc, char *argv[])
+{
+    int n;
+    char buf[BUFFSIZE];
+
+    while((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0) {
+        if(n != write(STDOUT_FILENO, buf, n)) {
+            err_sys("write error");
+        }
+    }
+
+    if(n < 0) {
+        err_sys("read error");
+    }
+
+    exit(0);
+}
+
